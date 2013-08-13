dd1a03fd8d778a049d95b7866ec4b2ec8a3df926
Andrew8xx8 <avk@8xx8.ru>
2013-08-14 01:01:50 +0400

    Own vimrc added

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -8,4 +8,4 @@ alias l="| less"
 # Most recently used commands
 alias mruc="history | awk '{CMD[$2]++;count++;}END { for (a in CMD)print CMD[a] " " CMD[a]/count*100 \"% \" a;}' | grep -v "./" | column -c3 -s \" \" -t | sort -nr | nl |  head -n10"
 
-alias tmux="TERM=screen-256color-bce tmux attach || TERM=screen-256color-bce tmux new"
+alias tmux="TERM=screen-256color-bce tmux"
