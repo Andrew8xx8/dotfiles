57062e21efcc65015d669b6431ef6ef02d7944a3
Andrew8xx8 <avk@8xx8.ru>
2013-10-23 12:15:50 +0400

    Advanced aliases added

--- system/git.zsh
+++ system/git.zsh
@@ -8,3 +8,14 @@ rc() {
 }
 
 alias gcim="g ci -m"
+alias gst="g st"
+alias gco="g co"
+alias gau="git add -u && git status"
+alias gam="git ci --amend"
+alias gbr="git branch -a"
+
+alias grs="git flow release start"
+alias grf="git flow release finish"
+
+alias gfs="git flow feature start"
+alias gff="git flow feature finish"
