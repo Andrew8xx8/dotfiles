9ebd4b9f63e80618c052d8ca47e5e95920e1c04a
Andreww8xx8 <avk@8xx8.ru>
2014-07-08 11:52:36 +0400

    Updated

--- system/env.zsh
+++ system/env.zsh
@@ -1,6 +1,3 @@
 export EDITOR=vim
 set t_Co=256
 export BUNDLE_JOBS=4
-
-export GOPATH=$HOME/go
-export PATH=$PATH:$GOPATH/bin
--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,7 @@
 export PATH=$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
 
+export GOPATH=$HOME/go
+export PATH=$PATH:$GOPATH/bin
+
+export PATH=/Users/8xx8/android-sdks/tools/:/Users/8xx8/android-sdks/platform-tools:$PATH
